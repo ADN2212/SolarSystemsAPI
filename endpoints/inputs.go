21dede01a4dab8b7ef71c85177d3a7b49433f450
Juan Adonis Nunez Collado <adn221294@gmail.com>
2025-08-12 21:26:37 -0400

    Add required tag to the Planet and Star strucst to the inputs in the endpoints package

--- endpoints/inputs.go
+++ endpoints/inputs.go
@@ -1,15 +1,15 @@
 package endpoints
 
 type Star struct {
-	Name      string `json:"name"`
-	SolarMass uint   `json:"solarMass"`
+	Name      string `json:"name" binding:"required"`
+	SolarMass uint   `json:"solarMass" binding:"required"`
 }
 
 type Planet struct {
-	Name      string `json:"name"`
-	Mass      int    `json:"mass"`
-	IsLibable bool   `json:"isLibable"`
-	StarId    uint   `json:"starId"`
+	Name      string `json:"name" binding:"required"`
+	Mass      int    `json:"mass" binding:"required"`
+	IsLibable bool   `json:"isLibable" binding:"required"`
+	StarId    uint   `json:"starId" binding:"required"`
 }
 
 type UpdatePlanetInput struct {
